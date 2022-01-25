b23899839afd3be7399dcb42630659e680301984
sergeigrom <morfeysg@gmail.com>
2022-01-25 20:41:41 +1000

    NewProperties

---
+++ src/ru/dvfu/mrcpk/examples/example_05/NewProperties.java
@@ -0,0 +1,11 @@
+package ru.dvfu.mrcpk.examples.example_05;
+
+public class NewProperties {
+    public static void main(String[] args) {
+        Integer j=71;
+        Integer k =++j;
+        int i=2;
+        k= i+j+k;
+        System.out.println(+k);
+    }
+}
